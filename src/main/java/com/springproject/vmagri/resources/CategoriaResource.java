c03d1b52dbad5544b7fc8f6d3ba1c0106f5f76d3
Viviane Erdoeg <vmerdoeg@outlook.com>
2018-09-17 15:21:57 -0300

    Firts Domain Class - Categoria

--- src/main/java/com/springproject/vmagri/resources/CategoriaResource.java
+++ src/main/java/com/springproject/vmagri/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.springproject.vmagri.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springproject.vmagri.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST is working!";
+//	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
+	public List<Categoria> list() {
+		
+		Categoria cat1 = new Categoria (1, "Informatica");
+		Categoria cat2 = new Categoria (2, "Escritorio");
+		List<Categoria> listCategoria = new ArrayList<Categoria>();
+		listCategoria.add(cat1);
+		listCategoria.add(cat2);
+		
+		return listCategoria;
 	}
 
 }
