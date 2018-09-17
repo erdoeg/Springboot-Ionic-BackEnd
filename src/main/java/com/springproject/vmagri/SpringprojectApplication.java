94fedad0a8fde0091a0aca39b3c83c36e0bcef6a
Viviane Erdoeg <vmerdoeg@outlook.com>
2018-09-17 13:15:02 -0300

    Starting Spring Boot Project

---
+++ src/main/java/com/springproject/vmagri/SpringprojectApplication.java
@@ -0,0 +1,12 @@
+package com.springproject.vmagri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectApplication.class, args);
+	}
+}
