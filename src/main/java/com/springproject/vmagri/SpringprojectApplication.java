6532ab88c56c49e89668b2a377930e775c8e19d4
Viviane Erdoeg <vmerdoeg@outlook.com>
2018-09-17 22:30:12 -0300

    Add domain Produto - many-to-many relationship with Categoria

--- src/main/java/com/springproject/vmagri/SpringprojectApplication.java
+++ src/main/java/com/springproject/vmagri/SpringprojectApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springproject.vmagri.domain.Categoria;
+import com.springproject.vmagri.domain.Produto;
 import com.springproject.vmagri.repositories.CategoriaRepository;
+import com.springproject.vmagri.repositories.ProdutoRepository;
+
 
 @SpringBootApplication
 public class SpringprojectApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepo;
+	
+	@Autowired
+	private ProdutoRepository produtoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringprojectApplication.class, args);
@@ -26,7 +32,19 @@ public class SpringprojectApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 }
