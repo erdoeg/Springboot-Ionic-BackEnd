88ee5f66a6b304b8578101122724f6224a5743bc
Viviane Erdoeg <vmerdoeg@outlook.com>
2018-10-18 00:20:16 -0300

    Categoria Pagination with filter parameters

--- src/main/java/com/springproject/vmagri/SpringprojectApplication.java
+++ src/main/java/com/springproject/vmagri/SpringprojectApplication.java
@@ -71,6 +71,12 @@ public class SpringprojectApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
+		Categoria cat3 = new Categoria(null, "cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "eletronicos");
+		Categoria cat5 = new Categoria(null, "jardinagem");
+		Categoria cat6 = new Categoria(null, "decorcao");
+		Categoria cat7 = new Categoria(null, "perfumaria");
+
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -84,7 +90,7 @@ public class SpringprojectApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
