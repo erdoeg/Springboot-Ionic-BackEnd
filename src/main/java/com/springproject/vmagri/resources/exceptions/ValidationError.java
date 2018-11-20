5e1d0e46654edeccfcebddd0dcf710fff02ec85d
Viviane Erdoeg <vmerdoeg@outlook.com>
2018-11-20 16:45:58 -0200

    Custom error message for validatiton exception

---
+++ src/main/java/com/springproject/vmagri/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.springproject.vmagri.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long dateTime) {
+		super(status, msg, dateTime);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		this.errors.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+}
